753df55f7721dbc6d6f1db8d531c0e3739856544
luizDiSouz1 <lair101112@gmail.com>
2024-01-23 12:01:48 -0300

    POO introdução ao programação a objeto

---
+++ src/pooTurma/Turma.java
@@ -0,0 +1,27 @@
+package pooTurma;
+
+public class Turma {
+	//Atributos da classe
+	
+	String  nome;
+	String telefone;
+	String email;
+	
+	//construtor
+	public Turma() {
+		System.out.println("___________________________________________________________");
+	}
+	
+	//Métodos
+	void ligarPc() {
+		System.out.println("Computador ligado");
+	}
+	
+	void logar() {
+		System.out.println("Login feito com sucesso");
+	}
+	
+	void desligarPc() {
+		System.out.println("Computador desligado");
+	}
+}
